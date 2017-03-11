e642b2a5a8a07886b5c89aa9ab7b33f1735a357b
Chong WANG <alex@Chongs-Mac-Pro.local>
2017-03-11 10:52:35 +0800

    on March 11 2017

---
+++ Cookbook/Chapter 1.R
@@ -0,0 +1,13 @@
+## This is the code created in reading Ch1 Ch2 of #RBook Gohil
+## Created: Alex Wang
+## Last update: March 11, 2017
+
+
+
+x=c(1:10)
+y=c(1:10)
+par(bg = "#646989", las = 1, col.lab = "black", col.axis = "white",bty = "n",cex.axis = 0.9,cex.lab= 1.5)
+plot(x,y, pch = 20, xlab = "fake x data", ylab = "fake y data")
+
+install.packages("ISLR")
+library(ISLR)
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Learn R for visualization
 
 ##Packages
+* googleVis
+* iPlots
 * RMySQL
+* ggplot
+
+
+##Book for learning
+
+* #RBook Gohil *R data visualization cookbook : over 80 recipes to analyze data and create stunning visualizations with R* / Atmajitsinh Gohil.
+
+* #Java *Data Visualization with JavaScript* / Stephen A. Thomas
+
+*
